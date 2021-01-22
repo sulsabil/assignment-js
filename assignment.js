2c3986befe6a295c23609b1f42d7a6740f0754e5
SABILA <salsabilnishat@gmail.com>
2021-01-22 21:40:23 +0600

    first commit

---
+++ assignment.js
@@ -0,0 +1,53 @@
+
+
+
+
+function kilometerToMeter(meter){
+    var kilometer = meter * 1000;
+    return kilometer;
+
+}
+
+var meter1 = kilometerToMeter(2.2);
+console.log(meter1);
+
+
+
+function hotelCost(x){
+    if(x >=1 && x <= 10){
+        return x * 100;
+    }
+    else if(x >= 11 && x <= 20){
+        return 10 * 100 + (x -10) * 80;
+    }
+    else if (x >= 21){
+        return (10 * 100) + (10 * 80) + (x - 20) * 50;
+    }
+        
+}
+
+
+function budgetCalculator(watch,phone,laptop){
+    var price = (watch*50) + (phone*100) + (laptop*500);
+    return (price);
+}
+
+var cost = budgetCalculator(2,1,1);
+console.log(cost);
+
+function megaFriend(frn){
+    var maxWord = frn[0];
+    for(var i =0; i < frn.length; i++){
+        var element = frn[i];
+        if(element.length > maxWord.length){
+            maxWord = element;
+
+            return element;
+        }
+    }
+}
+
+var result = megaFriend(['sabila', 'saha', 'mashroor']);
+console.log(result);
+
+
